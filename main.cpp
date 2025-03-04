2cb3bd41d9dbfff94ce30e282ec143eac256ca57
luis <luisgustavosr333@gmail.com>
2025-03-04 16:26:10 -0300

    main corrigido para array de Lista.

--- main.cpp
+++ main.cpp
@@ -5,35 +5,36 @@
 using namespace std;
 
 int main() {
-    Lista<int> player1, player2, baralho1, baralho2;
-    cria(player1);
-    cria(player2);
-    cria(baralho1);
-    cria(baralho2);
+    Lista<int> player[2], baralho[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cria(player[i]);
+        cria(baralho[i]);
+    }
 
     for (int i = 1; i < 101; i++)
     {
-        insere(baralho1, i, baralho1.cardinalidade + 1);
-        insere(baralho2, i, baralho2.cardinalidade + 1);
+        insere(baralho[0], i, baralho[0].cardinalidade + 1);
+        insere(baralho[1], i, baralho[1].cardinalidade + 1);
     }
 
     srand(time(NULL));
     for (int i = 0; i < 5; i++)
     {
-        int j = rand() % baralho1.cardinalidade + 1;
+        int j = rand() % baralho[0].cardinalidade + 1;
 
-        insere(player1, retornaElemento(baralho1, j), player1.cardinalidade + 1);
-        retira(baralho1, j);
+        insere(player[0], retornaElemento(baralho[0], j), player[0].cardinalidade + 1);
+        retira(baralho[0], j);
 
-        j = rand() % baralho1.cardinalidade + 1;
+        j = rand() % baralho[0].cardinalidade + 1;
 
-        insere(player2, retornaElemento(baralho1, j), player2.cardinalidade + 1);
-        retira(baralho1, j);
+        insere(player[1], retornaElemento(baralho[0], j), player[1].cardinalidade + 1);
+        retira(baralho[0], j);
 
     }
 
-    mostra(player1);
-    mostra(player2);
+    mostra(player[0]);
+    mostra(player[1]);
     
     int rodadas;
     cout << "/n digite o numero de rodadas a ser jogado:";
@@ -47,11 +48,11 @@ int main() {
 
     //verificar ganhador
     
-
-    destroi(player1);
-    destroi(player2);
-    destroi(baralho1);
-    destroi(baralho2);
-
+    for (int i = 0; i < 2; i++)
+    {
+        destroi(player[i]);
+        destroi(baralho[i]);
+    }
+    
     return 0;
 }
